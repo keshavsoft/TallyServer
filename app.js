94de371a70ae0603773a995f14e8b10f09e57d77
Satish Addanki <satishaddanki321@gmail.com>
2023-11-09 18:43:10 +0530

    satish

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const xml2js = require('xml2js')
 const app = express();
 
-app.use(express.text({limit: '50mb'}));
+app.use(express.text({ limit: '50mb' }));
 
 app.use(express.json());
 
@@ -17,19 +17,16 @@ app.get('/Tally', function (req, res) {
 
 app.post('/TallyPost', function (req, res) {
     let BodyData = req.body;
-    console.log("body",BodyData);
+    console.log("body", BodyData);
     BodyData.k1 = 916;
     res.json(BodyData);
 });
 
 app.post('/TallyPostText', function (req, res) {
     // console.log("req-------------",req.body);
-    fs.writeFileSync("TallyNew.xml",req.body);
+    fs.writeFileSync("TallyNew.xml", req.body);
 
-    var parseString = require('xml2js').parseString;
-    parseString(req.body, function (err, result) {
-        console.log(result.ENVELOPE.BODY[0].DATA[0].COLLECTION[0].LEDGER[0]["$"].NAME);
-    });
+    ToJson({ inBodyData: req.body });
 
     res.end(req.body.toString());
 });
@@ -40,3 +37,14 @@ app.listen(port, () => {
     console.log(`Listening in port : ${port}`);
     console.log(`Click to open : http://localhost:${port}`);
 });
+
+let ToJson = ({ inBodyData }) => {
+    var parseString = require('xml2js').parseString;
+    parseString(inBodyData, function (err, result) {
+        let DataNeeded = result.ENVELOPE.BODY[0].DATA[0].COLLECTION[0].LEDGER;
+        let ArrayData = DataNeeded.map(element => element["$"].NAME);
+        fs.writeFileSync("AccountNames.json", JSON.stringify(ArrayData));
+
+        console.log(ArrayData);
+    });
+};
