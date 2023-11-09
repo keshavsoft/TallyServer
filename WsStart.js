88ee625c335d87095a1e2bbea6047acb09772f04
Keshav <nknnkumar@yahoo.com>
2023-11-09 23:48:21 +0530

    keshav

--- WsStart.js
+++ WsStart.js
@@ -1,6 +1,9 @@
 const WebSocket = require('ws');
 
 let wss;
+const clients = new Map();
+const clientsInfo = new Map();
+
 
 let StartFunc = (server) => {
 
@@ -10,7 +13,28 @@ let StartFunc = (server) => {
 
 };
 
+function uuidv4() {
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+};
+
+
+let insertToClients = ({ inClients, ws }) => {
+    const id = uuidv4();
+    const color = Math.floor(Math.random() * 360);
+    const metadata = { id, color };
+
+    inClients.set(ws, metadata);
+
+};
+
 let WsOnConnection = (ws, req) => {
+    insertToClients({
+        inClients: clients,
+        ws
+    });
 
     const ip = req.socket.remoteAddress;
     ws.send(ip);
@@ -20,9 +44,18 @@ let WsOnConnection = (ws, req) => {
 
         message.inComingMessage = messageAsString.toString();
 
-       console.log("message",messageAsString.toString());
+        console.log("message", messageAsString.toString());
 
-       
+        for (let [key, value] of clients) {
+            console.log(value);
+        };
+
+
+        const metadata = clients.get(ws);
+
+
+        console.log("metadata", metadata);
+
     });
 
     ws.on('close', () => {
