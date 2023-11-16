134bfd9aa33d1c07a29cef095436ed80631fc41a
Satish Addanki <satishaddanki321@gmail.com>
2023-11-16 18:24:13 +0530

    satish

--- ForWebSocket/OnMessage.js
+++ ForWebSocket/OnMessage.js
@@ -2,7 +2,6 @@ let CommonAsJson = require('./ReceiveMessage/AsJson');
 let CommonAsString = require('./ReceiveMessage/AsString');
 
 let StartFunc = ({ inMessageAsString, inClients, inws, inwss }) => {
-
     const metadata = inClients.get(inws);
     try {
         let LocalJsonData = JSON.parse(inMessageAsString.toString());
--- ForWebSocket/PrivateMessage.js
+++ ForWebSocket/PrivateMessage.js
@@ -9,17 +9,32 @@ let SendToAll = ({ inwss, inmessage }) => {
 
 }
 
-let SendSingle = ({ inws, inmessage, inReceiver, inClients }) => {
-    inClients.forEach(function each(client) {
-        if (client.Name === inReceiver) {
-            console.log("sent",inmessage);
+let SendSingle = ({ inwss, inmessage, inReceiver, inClients }) => {
+ for (let [key, value] of inClients) {
+            if (key.readyState === WebSocket.OPEN && value.Name === inReceiver) {
+                console.log("Matched",[key,value]);
+                console.log("log",inwss.clients.get([key,value]));
+            }
+        };
+};
+    //     }); {
+    //         console.log("cli",client);
+    //     // for (let [key, value] of inClients) {
+    //     //     if (key.readyState === WebSocket.OPEN && value.Name === inReceiver) {
+    //     //         console.log("Matched",[key,value]);
+    //     //     }
+    //     // };
+    // };
+    // inClients.forEach(function each(client) {
+    //     console.log("cli",client);
+    //     if (client.Name === inReceiver) {
+    //         console.log("sent",client);
 
-            inws.send(inmessage);
-            console.log("delivered",inmessage);
+    //         console.log("client",inwss.clients.get(client));
 
-        }
-    });
+    //         inwss.clients.get(client).send(inmessage);
+    //         console.log("delivered",inmessage);
 
-}
+    //     }
 
-module.exports = SendSingle;
+    module.exports = SendSingle;
